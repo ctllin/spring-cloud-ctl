107ef9ac00c10f0511f719f41898df48d31b08ef
林家小院 <guolinit@163.com>
2018-12-17 12:51:09 +0800

    init

---
+++ src/main/java/com/ctl/spring/cloud/springcloudctl/SpringCloudCtlApplication.java
@@ -0,0 +1,14 @@
+package com.ctl.spring.cloud.springcloudctl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudCtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudCtlApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/ctl/spring/cloud/springcloudctl/SpringCloudCtlApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ctl.spring.cloud.springcloudctl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudCtlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
